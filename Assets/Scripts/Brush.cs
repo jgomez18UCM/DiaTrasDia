9ecd6548a8d42e0719358a5e22c49b06b9237a29
Tomás López Antón <tomalope@ucm.es>
2022-12-14 00:08:03 +0100

    Mecanicas de maquillaje

---
+++ Assets/Scripts/Brush.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+
+public class Brush : MonoBehaviour
+{
+    public PaintMakeup makeup;
+    [SerializeField] Color color;
+    [Range(0.1f, 0.6f)] public float radius;
+    [Range(0.1f, 1f)] public float harshness;
+    [SerializeField] int currentBrush;
+    [SerializeField] int numberOfPoints;
+
+    [SerializeField] float z;
+
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.material = Resources.Load("Blurry", typeof(Material)) as Material;
+        line.material.color = Color.black;
+        line.positionCount = numberOfPoints;
+        line.loop = false;
+        line.startWidth = 0.05f;
+        line.endWidth = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        line.positionCount = numberOfPoints;
+        for (int j = 0; j < numberOfPoints; j++) 
+        {
+            line.SetPosition(j, transform.position + new Vector3(radius * Mathf.Cos(j * (360 / numberOfPoints)), radius * Mathf.Sin(j * (360 / numberOfPoints)), z));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            makeup.setPainting(true);
+            int layerMask = 1 << 11;
+
+            if (Physics2D.Raycast(transform.position, new Vector3(0, 0, 1), 1000f, layerMask))
+            {
+                makeup.paintPixel(transform.position, color, radius, harshness, false);
+            }
+
+            int steps = 3;
+
+            for (int j = 1; j <= steps; j++)
+            {
+                float currentRadius = (j / (float)steps) * radius;
+
+                for (int i = 0; i < 36; i++)
+                {
+                    Vector3 pos = transform.position + new Vector3(currentRadius * Mathf.Cos(i * 10), currentRadius * Mathf.Sin(i * 10));
+
+                    if (Physics2D.Raycast(pos, new Vector3(0, 0, 1), 1000f, layerMask))
+                    {
+                        makeup.paintPixel(pos, color, radius, harshness, steps == j);
+                    }
+                }
+            }
+        }
+
+        else makeup.setPainting(false);
+    }
+}
--- Assets/Scripts/PaintMakeup.cs
+++ Assets/Scripts/PaintMakeup.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PaintMakeup : MonoBehaviour
 {
     [SerializeField] Texture2D makeupTexture;
+    public bool painting = false;
     Renderer renderer;
+    [SerializeField] public float[,] lerpValues;
     // Start is called before the first frame update
     void Start()
     {
-        makeupTexture = new Texture2D(128, 128);
+        lerpValues = new float[256, 256];
+        
+        for(int i = 0; i < 256; i++) 
+        {
+            for (int j = 0; j < 256; j++) lerpValues[i, j] = 0.0f;
+        }
+
+        makeupTexture = new Texture2D(256, 256);
         renderer = GetComponent<Renderer>();
         renderer.material.mainTexture = makeupTexture;
 
@@ -17,7 +26,7 @@ public class PaintMakeup : MonoBehaviour
         {
             for (int x = 0; x < makeupTexture.width; x++)
             {
-                Color color = ((x & y) != 0 ? Color.white : Color.gray);
+                Color color = new Color(0, 0, 0, 0);
                 makeupTexture.SetPixel(x, y, color);
             }
         }
@@ -29,5 +38,68 @@ public class PaintMakeup : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void paintPixel(Vector2 pos, Color c, float radius, float harshness, bool edge) 
+    {
+        Bounds bounds = gameObject.GetComponent<MeshFilter>().mesh.bounds;
+        Vector2 pixelSize = new Vector2(bounds.size.x / (float)makeupTexture.width, bounds.size.z / (float)makeupTexture.height);
+        Vector2 deltaPos = new Vector2(transform.position.x - pos.x, transform.position.y - pos.y);
+
+        int x = (int)(deltaPos.x / pixelSize.x);
+        int y = (int)(deltaPos.y / pixelSize.y);
+        x = (makeupTexture.width / 2) + x % makeupTexture.width;
+        y = (makeupTexture.height / 2) + y % makeupTexture.height;
+
+        Color color;
+
+        int area = (int)((radius / 4) / pixelSize.x);
+
+        if (!edge)
+        {
+            for (int i = -area; i <= area; i++)
+            {
+                for (int j = -area; j <= area; j++)
+                {
+                    Color pixelColor = makeupTexture.GetPixel(x + i, y + j);
+
+                    if (pixelColor.a != 0 && (c.r != pixelColor.r || c.g != pixelColor.g || c.b != pixelColor.b))
+                    {
+                        lerpValues[x + i, y + j] += harshness / 1000f;
+                        color = Color.Lerp(pixelColor, c, lerpValues[x + i, y + j]);
+                    }
+                    else color = c;
+                    makeupTexture.SetPixel(x + i, y + j, color);
+                }
+            }
+        }
+
+        else
+        {
+            Color pixelColor = makeupTexture.GetPixel(x, y);
+
+            if (pixelColor.a != 0 && (c.r != pixelColor.r || c.g != pixelColor.g || c.b != pixelColor.b))
+            {
+                lerpValues[x, y] += harshness / 1000f;
+                color = Color.Lerp(pixelColor, c, lerpValues[x, y]);
+            }
+            else color = c;
+            makeupTexture.SetPixel(x, y, color);
+        }
+
+        makeupTexture.Apply();
+    }
+
+    public void setPainting(bool b) 
+    {
+        painting = b;
+
+        if(!painting) 
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++) lerpValues[i, j] = 0.0f;
+            }
+        }
     }
 }
